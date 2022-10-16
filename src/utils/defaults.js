b192d443b5c0867404178453e5d7be32223dc8ed
juelhossain <juelhossainnn@gmail.com>
2022-10-17 02:09:51 +0600

    koyeb site added

--- src/utils/defaults.js
+++ src/utils/defaults.js
@@ -1,3 +1,3 @@
-export const apiURL = "https://chat-application-assignment.herokuapp.com";
+export const apiURL = "https://chat-application-redux-juelhossain.koyeb.app";
 export const conversationPerPage = 12;
 export const messagePerPage = 16;
